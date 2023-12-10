d77c520205189611cdc58fdd6a09e5c032037927
MysteriousAcadia <mysteriousacadia@gmail.com>
2023-12-10 08:36:10 +0530

    Add build

---
+++ dist/assets/index-17rNcjxN.js
@@ -0,0 +1,9 @@
+import{bb as t,aJ as e,bf as i,bg as n,bi as s,bc as f,bh as d,be as c,bd as l}from"./index--5PAUoZo.js";const a=`
+@font-face {
+  font-family: 'Inter';
+  font-style:  normal;
+  font-weight: 300 600;
+  font-display: swap;
+  src: url("https://rsms.me/inter/font-files/Inter-roman.var.woff2?v=3.19") format("woff2");
+}
+`;export{a as InterVar,t as ProviderRpcError,e as ProviderRpcErrorCode,i as chainIdValidation,n as chainNamespaceValidation,s as chainValidation,f as createEIP1193Provider,d as providerConnectionInfoValidation,c as validate,l as weiToEth};
